255fed79ce4003fa1c52a773e2e531077086f57c
BadhonRahman01 <badhon.rahman184@gmail.com>
2023-02-25 22:11:51 +0600

    9.5

---
+++ third-project/src/components/UserInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <base-card>
+    <div>
+      <h3>{{ fullName }}</h3>
+      <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+    </div>
+    <p>{{ infoText }}</p>
+  </base-card>
+  </section>
+</template>
+
+<script scoped>
+export default {
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
+
+<style scoped>
+
+section div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
