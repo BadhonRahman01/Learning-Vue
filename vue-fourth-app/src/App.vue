58e2d9feb21f3a5360c7d8cdc949c3490504850b
BadhonRahman01 <badhon.rahman184@gmail.com>
2023-03-04 23:43:51 +0600

    10.5

---
+++ vue-fourth-app/src/App.vue
@@ -0,0 +1,59 @@
+<template>
+  <the-header title="RememberMe"></the-header>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
+
+<script>
+// import LearningResource from "./components/learning-resources/LearningResource.vue";
+import StoredResources from "./components/learning-resources/StoredResources.vue";
+import TheHeader from './components/layouts/TheHeader.vue';
+
+export default {
+  components: {
+    StoredResources,
+    TheHeader
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description: "The oficial VUE Documentation",
+          link: "vuejs.org"
+        },
+        {
+          id: "google",
+          title: "Google",
+          description: "Learn to goole",
+          link: "google.com"
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
