3559f99cbc1d4d80139c512eb249b24640c9b716
BadhonRahman01 <badhon.rahman184@gmail.com>
2023-04-01 22:46:01 +0600

    11Done

---
+++ vue-fifth-app/src/components/TheForm.vue
@@ -0,0 +1,185 @@
+<template>
+  <form @submit.prevent="submitForm">
+    <div class="form-control" :class="{invalid: useNameValidity === 'invalid'}">
+      <label for="user-name">Your Name</label>
+      <input id="user-name" name="user-name" type="text" v-model.trim="userName" @blur="validateInput"/>
+      <p v-if=" useNameValidity === 'invalid'">Please entera valid Name</p>
+    </div>
+    <div class="form-control">
+      <label for="age">Your Age (Years)</label>
+      <input id="age" name="age" type="number" v-model.number="userAge"/>
+    </div>
+    <div class="form-control">
+      <label for="referrer">How did you hear about us?</label>
+      <select id="referrer" name="referrer" v-model="referrer">
+        <option value="google">Google</option>
+        <option value="wom">Word of mouth</option>
+        <option value="newspaper">Newspaper</option>
+      </select>
+    </div>
+    <div class="form-control">
+      <h2>What are you interested in?</h2>
+      <div>
+        <input id="interest-news" name="interest" type="checkbox" value="news" v-model="interest"/>
+        <label for="interest-news">News</label>
+      </div>
+      <div>
+        <input id="interest-tutorials" name="interest" type="checkbox" value="tutorials" v-model="interest"/>
+        <label for="interest-tutorials">Tutorials</label>
+      </div>
+      <div>
+        <input id="interest-nothing" name="interest" type="checkbox" value="nothing" v-model="interest"/>
+        <label for="interest-nothing">Nothing</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <h2>How do you learn?</h2>
+      <div>
+        <input id="how-video" name="how" type="radio" value="video" v-model="how"/>
+        <label for="how-video">Video Courses</label>
+      </div>
+      <div>
+        <input id="how-blogs" name="how" type="radio" value="blogs" v-model="how" />
+        <label for="how-blogs">Blogs</label>
+      </div>
+      <div>
+        <input id="how-other" name="how" type="radio" value="other" v-model="how"/>
+        <label for="how-other">Other</label>
+      </div>
+    </div>
+    <div class="form-control">
+      <rating-control v-model="rating"></rating-control>
+    </div>
+    <div class="form-control">
+      <input type="checkbox" id="confirm-terms" name="confirm-terms" v-model="confirm"/>
+      <label for="confirm-terms">I agree to the terms and conditions</label>
+    </div>
+    <div>
+      <button>Save Data</button>
+    </div>
+  </form>
+</template>
+
+<script>
+import RatingControl from './RatingControl.vue';
+
+export default {
+  components: {
+    RatingControl
+  },
+  data() {
+    return {
+      userName: '',
+      userAge: null,
+      referrer: 'wom',
+      interest: [],
+      how: null,
+      confirm: false,
+      rating: null,
+      useNameValidity: 'pending',
+    };
+  },
+  methods:{
+    submitForm(){
+      // const formData = new FormData(event.target);
+      // const data = Object.fromEntries(formData.entries());
+      console.log('UserName:' + this.userName);
+      this.userName = '';
+      console.log(this.userAge);
+      this.userAge = null;
+      console.log('Ref:'+this.referrer);
+      this.referrer = 'wom';
+      console.log('Interest:');
+      console.log(this.interest);
+      console.log('How:');
+      console.log(this.how);
+      this.interest = [];
+      this.how = null;
+      console.log('Confirm:');
+      console.log(this.confirm);
+      this.confirm = false;
+      console.log('Rating:');
+      console.log(this.rating);
+      this.rating = null;
+    },
+    validateInput(){
+      if(this.userName.trim() === ''){
+        this.useNameValidity = 'invalid';
+      }else{
+        this.useNameValidity = 'valid';
+      }
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+form {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: #ffffff;
+}
+  .form-control.invalid input {
+    border-color: #d73a49;
+    background-color: #ffd7d9;
+  }
+  .form-control.invalid label {
+    color: #d73a49;
+  }
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+}
+
+h2 {
+  font-size: 1rem;
+  margin: 0.5rem 0;
+}
+
+input,
+select {
+  display: block;
+  width: 100%;
+  font: inherit;
+  margin-top: 0.5rem;
+}
+
+select {
+  width: auto;
+}
+
+input[type='checkbox'],
+input[type='radio'] {
+  display: inline-block;
+  width: auto;
+  margin-right: 1rem;
+}
+
+input[type='checkbox'] + label,
+input[type='radio'] + label {
+  font-weight: normal;
+}
+
+button {
+  font: inherit;
+  border: 1px solid #0076bb;
+  background-color: #0076bb;
+  color: white;
+  cursor: pointer;
+  padding: 0.75rem 2rem;
+  border-radius: 30px;
+}
+
+button:hover,
+button:active {
+  border-color: #002350;
+  background-color: #002350;
+}
+</style>
