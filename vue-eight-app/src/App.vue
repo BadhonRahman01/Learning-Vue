55f8d674bc4c1ff2e03700881c8b1eb3ad803ab5
BadhonRahman01 <badhon.rahman184@gmail.com>
2023-04-09 23:32:46 +0600

    14.14Done

--- vue-eight-app/src/App.vue
+++ vue-eight-app/src/App.vue
@@ -1,33 +1,112 @@
 <template>
   <div class="container">
-    <div class="block" :class="{animate: animatedBlock}"></div>
-    <button @click="animateBlock" >Animate</button>
+    <div class="block" :class="{ animate: animatedBlock }"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <div class="container">
-    <transition>
+    <transition
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @enter-cancelled="enterCancelled"
+      @leave-cancelled="leaveCancelled"
+    >
       <p v-if="paraIsVisible">This is sometimes only visible..</p>
     </transition>
     <button @click="toggleParagraph">Toggle Paragraph</button>
   </div>
-  <base-modal @close="hideDialog" v-if="dialogIsVisible">
+  <div class="container">
+    <transition name="buttons-fade" mode="out-in">
+      <button @click="showUsers" v-if="!usersAreVisible">Show Users</button>
+      <button @click="hideUsers" v-else>Hide Users</button>
+    </transition>
+  </div>
+  <base-modal @close="hideDialog" :open="dialogIsVisible">
     <p>This is a test dialog!</p>
     <button @click="hideDialog">Close it!</button>
   </base-modal>
+
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
+  </div>
+  <div class="container">
+    <users-data></users-data>
   </div>
 </template>  
 
 <script>
+import UsersData from "./components/UsersData.vue";
 export default {
+  components: {
+    UsersData,
+  },
   data() {
-    return { 
+    return {
       animatedBlock: false,
       dialogIsVisible: false,
       paraIsVisible: false,
-     };
+      usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null,
+    };
   },
   methods: {
+    beforeEnter(el) {
+      console.log("beforeEnter");
+      console.log(el);
+      el.style.opacity = 0;
+    },
+    enter(el, done) {
+      console.log("enter");
+      let round = 1;
+      this.enterInterval = setInterval(() =>{
+        el.style.opacity = round* 0.01;
+        round++;
+        if(round > 100){
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterEnter() {
+      console.log("afterEnter");
+    },
+    leave(el,done) {
+      console.log("leave");
+      let round = 1;
+      this.leaveInterval = setInterval(() =>{
+        el.style.opacity = 1 - round* 0.01;
+        round++;
+        if(round > 100){
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
+    },
+    afterLeave() {
+      console.log("afterLeave");
+    },
+    beforeLeave() {
+      console.log("beforeLeave");
+    },
+    enterCancelled() {
+      console.log("enterCancelled");
+      clearInterval(this.enterInterval);
+    },
+    leaveCancelled() {
+      console.log("leaveCancelled");
+      clearInterval(this.leaveInterval);
+    },
+    showUsers() {
+      this.usersAreVisible = true;
+    },
+    hideUsers() {
+      this.usersAreVisible = false;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -89,42 +168,70 @@ button:active {
   border: 2px solid #ccc;
   border-radius: 12px;
 }
-.animate{
+.animate {
   /* transform: translateX(-150px); */
-  animation: slide-fade 0.5s ease-in-out forwards;
-}
-.v-enter-from{
-opacity: 0;
-transform: translateY(-30px);
-}
-.v-enter-active{
-  transition: all 0.5s ease;
-}
-.v-enter-to{
-  opacity: 1;
-  transform: translateY(0px);
-}
-.v-leave-from{
-  opacity: 1;
-  transform: translateY(0px);
-}
-.v-leave-active{
-  transition: all 0.5s ease;
-}
-.v-leave-to{
-  opacity: 0;
-  transform: translateY(-30px);
+  animation: slide-scale 0.5s ease-in-out forwards;
 }
 
-@keyframes slide-fade {
-  0%{
+.buttons-fade-enter-from,
+.buttons-fade-leave-to {
+  opacity: 0;
+}
+.buttons-fade-enter-active {
+  transition: opacity 0.5s ease-out;
+}
+.buttons-fade-leave-active {
+  transition: opacity 0.5s ease-in;
+}
+.buttons-fade-enter-to,
+.buttons-fade-leave-from {
+  opacity: 1;
+}
+
+/* @keyframes modal {
+  from{
+   opacity: 0;
+    transform: translateY(-50px) scale(0.8);; 
+  }
+  to{
+    opacity: 1;
+    transform: translateY(0) scale(1);
+  }
+} */
+@keyframes slide-scale {
+  0% {
     tramsform: translateX(0) scale(1);
   }
-  70%{
+  70% {
     transform: translateX(-120px) scale(1.1);
   }
-  100%{
+  100% {
     transform: translateX(-150px) scale(1);
   }
 }
 </style>
+
+.para-enter-from {
+  /* opacity: 0;
+transform: translateY(-30px); */
+}
+.para-enter-active {
+  /* transition: all 0.5s ease; */
+  animation: slide-scale 0.5s ease-in-out;
+}
+.para-enter-to {
+  /* opacity: 1;
+  transform: translateY(0px); */
+}
+.para-leave-from {
+  /* opacity: 1;
+  transform: translateY(0px); */
+}
+.para-leave-active {
+  /* transition: all 0.5s ease; */
+  animation: slide-scale 0.5s ease-in-out;
+}
+.para-leave-to {
+  /* opacity: 0;
+  transform: translateY(-30px); */
+}
---
+++ vue-eight-app/src/components/UsersData.vue
@@ -0,0 +1,53 @@
+<template>
+    <ul>
+        <li v-for="user in users" :key="user" @click="removeuser(user)">{{ user }}</li>
+    </ul>
+    <input type="text" ref="username"/>
+    <button @click="addUser">Add User</button>
+</template>
+
+
+
+<script>
+export default {
+    data(){
+        return {
+            users: ['Badhon', 'Rahman', 'Shuvo', 'Rihan', 'Rinzo'],
+        };
+    },
+    methods: {
+        addUser(){
+            // const input = document.querySelector('input');
+            // const user = input.value;
+            // this.users.push(user);
+            // input.value = '';
+            const user = this.$refs.username.value;
+            this.users.unshift(user);
+        },
+        removeuser(user){
+            const index = this.users.indexOf(user);
+            this.users.splice(index, 1);
+            // this.users = this.users.filter(usr => usr !== user);
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+ul{
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+}
+li{
+    padding: 1rem;
+    border: 1px solid #ccc;
+    transition: transform 0.3s ease-out;
+}
+</style>
