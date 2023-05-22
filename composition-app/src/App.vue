a625ace520652fbd3f38a928e338701108c64c35
BadhonRahman01 <badhon.rahman184@gmail.com>
2023-05-22 22:14:35 +0600

    19.06

---
+++ composition-app/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="container">
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+  </section>
+</template>
+
+<script>
+// import { ref } from 'vue';
+import { reactive } from 'vue';//reactive only works with object
+
+export default {
+  setup(){
+    // const userName = ref('Badhon');
+    // const age = ref(25);
+    const user = reactive({
+      name: 'Badhon',
+      age: 25,
+    });
+    setTimeout(() => {
+      user.name = 'Badhon Khan';
+      user.age = 26;
+    }, 2000);
+    return { user };
+  }
+  // data() {
+  //   return {
+  //     userName: 'Badhon',
+  //   };
+  // },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
