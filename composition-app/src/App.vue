b22acf9c68030c4bf13b08ef7b8bb224211792a6
BadhonRahman01 <badhon.rahman184@gmail.com>
2023-05-23 22:32:46 +0600

    basicComposition

--- composition-app/src/App.vue
+++ composition-app/src/App.vue
@@ -1,27 +1,56 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <h2>{{ uName }}</h2>
+    <h3>{{ uAge }}</h3>
+    <button @click="setNewAge">Change Age</button>
+    <div>
+      <input type="text" placeholder="First Name" v-model="firstName">
+      <input type="text" placeholder="Last Name" v-model="lastName">
+    </div>
   </section>
 </template>
 
 <script>
-// import { ref } from 'vue';
-import { reactive } from 'vue';//reactive only works with object
+import { ref, computed,watch } from 'vue';
+// import { reactive } from 'vue';//reactive only works with object
 
 export default {
   setup(){
-    // const userName = ref('Badhon');
-    // const age = ref(25);
-    const user = reactive({
-      name: 'Badhon',
-      age: 25,
+    // const uName = ref('Badhon');
+    const uAge = ref(25);
+    const firstName = ref('');
+    const lastName = ref('');
+    // const user = reactive({
+    //   name: 'Badhon',
+    //   age: 25,
+    // });
+    // setTimeout(() => {
+    //   user.name = 'Badhon Khan';
+    //   user.age = 26;
+    // }, 2000);
+
+    const uName = computed(function(){
+      // console.log('Running again...');
+      // return `${firstName.value} ${lastName.value}`
+      return firstName.value + ' ' + lastName.value;
     });
-    setTimeout(() => {
-      user.name = 'Badhon Khan';
-      user.age = 26;
-    }, 2000);
-    return { user };
+    watch([uAge, uName], function(newValues, oldValues){
+      console.log('Old Age' + oldValues[0]);
+      console.log('New Age' + newValues[0]);  
+      console.log('Old name' + oldValues[1]);
+      console.log('New name' + newValues[1]);  
+    });
+    function setNewAge() {
+      uName.value = 'Badhon Khan';
+      uAge.value = 28;
+    }
+    function setFirstName(event){
+      firstName.value = event.target.value
+    }
+    function setLastName(event){
+      lastName.value = event.target.value
+    }
+    return { uName, uAge, firstName, lastName,setNewAge ,setLastName,setFirstName };
   }
   // data() {
   //   return {
